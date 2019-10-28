c4a7c8361022fd9ef80da20e79ac38001b557afb
Bryan Fisher <brf2010@med.cornell.edu>
2019-10-28 14:12:51 -0400

    changes to make the script spider a URL instead of searching

--- getface.py
+++ getface.py
@@ -12,16 +12,17 @@ config = configparser.ConfigParser()
 config.read("config.conf")
 input_url = config[section]["url"]
 
+url_regex = re.compile('"https?://.*?"')
+
 #input_url = input("what url do you want to query: ")
 
-site_request = requests.get(input_url)
 
-site_data = site_request.text
 
 search_string = input("what do you want to search for: ")
 
-url_regex = re.compile('"https?://.*?"')
 
+site_request = requests.get(input_url)
+site_data = site_request.text
 for match in url_regex.findall(site_data):
     if search_string in match:
         print(match.strip('"'))
