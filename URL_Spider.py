8fc668b5236471055c3eee6344526956930d19bd
Ross Goldstein <rog2028@med.cornell.edu>
2019-12-23 14:37:55 -0500

    Latest edit

--- URL_Spider.py
+++ URL_Spider.py
@@ -11,42 +11,35 @@ parser.add_argument("input", help= "Specify a URL to search or a config file sec
 parser.add_argument("-s" , help="Search Sting")
 parser.add_argument("-c", help="Config File")
 args= parser.parse_args()
-print(args)
-print(args.input)
-print(args.s)
-print(args.c)
+# print(args)
+# print(args.input)
+# print(args.s)
+# print(args.c)
+
+
 
 if args.c is not None:
         config = configparser.ConfigParser()
         config.read(args.c)
         url = config[args.input]["url"]
-        print(url)
+        # print(url)
 
 else:
         url = args.input
-        print(url)
+        # print(url)
         
-
-sys.exit()
-
-#config = URL 
-#searchstring
-#config_file = Col_Gile.conf
-
-
+if args.s is None:
+        search_string = input("What do you want to search for? (If you want to match any URL, hit Enter): ")
+else:
+        search_string = args.s
 
 
 url_regex = re.compile('"https?://.*?"')
 
-#input_url = input("what url do you want to query: ")
 
-
-
-search_string = input("what do you want to search for: ")
-
-
-site_request = requests.get(input_url)
+site_request = requests.get(url)
 site_data = site_request.text
 for match in url_regex.findall(site_data):
     if search_string in match:
         print(match.strip('"'))
+
